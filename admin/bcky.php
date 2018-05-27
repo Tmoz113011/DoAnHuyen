94e1522b47f4b8b2ff26f08572b29ba75e96d5ab
Do Truong <tmoz113011@gmail.com>
2018-05-27 18:42:10 +0700

    Bao cao theo ky

---
+++ admin/bcky.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tmoz
+ * Date: 27/05/2018
+ * Time: 3:24 CH
+ */
+if ($_SESSION['login_us'] == 'ok' && !empty($_SESSION['quyen']) && in_array('2', $_SESSION['quyen'])) {
+    ?>
+    <div class="container">
+        <div class="title">
+            <h3 class="text-center">Báo cáo theo kì học</h3>
+        </div>
+        <div><a href="index.php?page=addbcky" class="btn btn-success">
+                Thêm mới báo cáo
+            </a></div>
+
+    </div>
+<?php } else {
+    header("location:index.php");
+} ?>
